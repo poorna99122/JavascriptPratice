fe2996e61440113ecb0901d3af3e828a4ac4ca66
poorna99122 <poornachandrarao99122@gmail.com>
2022-03-22 01:22:27 -0700

    First Commit

---
+++ script.js
@@ -0,0 +1,44 @@
+// function b(){
+//     var myVar;
+//     console.log("Inside b : "+myVar);
+// }
+
+// function a() {
+//     var myVar = 2;
+//     console.log("Inside a : "+myVar);
+//     b();
+// }
+
+// var myVar = 1;
+// console.log("Global : "+myVar);
+// a();
+
+
+
+//long running function
+// function waitThreeSeconds(){
+//     var ms = 3000 + new Date().getTime();
+//     while(new Date() < ms){
+//         console.log('finished function');
+//     }
+// }
+
+// function clickHandler(){
+//     console.log('click event');
+// }
+
+// //listen for the click event
+// document.addEventListener('click',clickHandler);
+
+
+// waitThreeSeconds();
+// console.log('finished execution');
+
+
+
+
+
+
+
+
+console.log(1<2<3);
